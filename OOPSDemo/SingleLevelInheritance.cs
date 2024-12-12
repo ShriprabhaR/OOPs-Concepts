d0fb643b5e0125f7dc9b28022b21fa72f826d4dc
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-12 16:39:32 +0530

    Remaining files

---
+++ OOPSDemo/SingleLevelInheritance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPSDemo
+{
+    internal class SingleLevelInheritance
+    {
+
+        class Person
+        {
+            int age = 22;
+            String name = "Shri";
+
+            public void Display()
+            {
+                Console.WriteLine("Name: " + name + " Age: " + age);
+            }
+        }
+
+        class Employee : Person
+        {
+            static void Main(string[] args)
+            {
+                Employee e = new Employee();
+                e.Display();
+            }
+
+        }
+    }
+}
