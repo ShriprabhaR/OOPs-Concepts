7ab5cbe402cf24ad18374c7270ad18f401de6216
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-12 15:13:57 +0530

    OOPs concepts

---
+++ OOPSDemo/PolymorphismCTP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPSDemo
+{
+    internal class PolymorphismCTP
+    {
+        static void Main(string[] args)
+        {
+            PolymorphismCTP poly = new PolymorphismCTP();
+            Console.WriteLine("Sum of 2 numbers is: "+ poly.Add(10, 20));
+
+            Console.WriteLine("Sum of 3 numbers is: "+ poly.Add(20, 30, 10));
+
+            Console.WriteLine("Sum of 2 numbers is: "+ poly.Add(10.5, 20.9));
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Overloaded method
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        //Overloaded
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ OOPSDemo/PolymorphismRTP.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPSDemo
+{
+    public class PolymorphismRTP
+    {
+        public virtual void Area()
+        {
+            Console.WriteLine("Area of shape ");
+        }
+
+        static void Main(string[] args)
+        {
+           PolymorphismRTP rtp;
+
+           rtp = new Circle();
+           rtp.Area();
+
+           rtp = new Square();
+           rtp.Area();
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+
+    public class Circle : PolymorphismRTP
+    {
+        
+        public override void Area() 
+        {
+            double pi = 3.142;
+            int radius = 20;
+            double area = pi * radius * radius;
+            Console.WriteLine("area of circle: "+area);
+
+        }
+
+    }
+
+    public class Square : PolymorphismRTP
+    {
+        public override void Area()
+        {
+            int length = 10;
+            int breadth = 15;
+            int area = length * breadth;
+            Console.WriteLine("area of Square: " + area);
+        }
+    }
+    
+}
